13627d1b7ed7aea83ceeaf2dc2f439bb83a169ce
henriquedry <35507359+henriquedry@users.noreply.github.com>
2018-05-18 20:57:59 -0300

    Update updateex.php

--- updateex.php
+++ updateex.php
@@ -12,18 +12,11 @@ $sql = "UPDATE series SET  id='$id ,status='$status' WHERE id=$id";
 
 if ($conn->query($sql) === TRUE) {
 
-?>       
 
-<section>
-    <div class="container">
-        <p class="h3">Produto Atualizado com sucesso</p>
-        <a href="perfilfunc.php" class="btn btn-secondary">Volta ao Perfil</a>
-        <a href="atualizaprod.php" class="btn btn-secondary">Atulizar mais Produtos</a>
+echo "atualizado com sucesso";
+}
 
-    </div>
 
-</section>
-                                                     
 
 
 <?php
